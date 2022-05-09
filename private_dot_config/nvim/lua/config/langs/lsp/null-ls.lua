000638f05cc0fcb6281cd8b05a078e7dc8ae59b4
hehelego <2364261262@qq.com>
2022-05-09 21:13:59 +0800

    upd(nvim): update null-ls.nvim diagnostics sources

--- private_dot_config/nvim/lua/config/langs/lsp/null-ls.lua
+++ private_dot_config/nvim/lua/config/langs/lsp/null-ls.lua
@@ -5,14 +5,25 @@ local formatting = null_ls.builtins.formatting
 local diagnostics = null_ls.builtins.diagnostics
 local code_actions = null_ls.builtins.code_actions
 
+local src = {}
+-- formatter
+src = vim.tbl_extend("force", src, {
+	formatting.yapf,
+	formatting.stylua,
+	formatting.rustfmt,
+	formatting.clang_format,
+})
+-- diagnostics
+src = vim.tbl_extend("force", src, {
+	diagnostics.pylint,
+	diagnostics.chktex,
+	diagnostics.fish,
+})
+-- code action
+src = vim.tbl_extend("force", src, {
+	code_actions.gitsigns,
+})
+
 null_ls.setup({
-	sources = {
-		formatting.yapf,
-		formatting.stylua,
-		formatting.rustfmt,
-		formatting.clang_format,
-		diagnostics.flake8,
-		diagnostics.chktex,
-		code_actions.gitsigns,
-	},
+	sources = src,
 })
