9cdf97672055d69a3724fb08376786004f156567
hehelego <2364261262@qq.com>
2022-05-15 19:15:14 +0800

    upd(nvim): lualine config

--- private_dot_config/nvim/lua/config/ui/lualine.lua
+++ private_dot_config/nvim/lua/config/ui/lualine.lua
@@ -1,8 +1,7 @@
 -- See <https://github.com/nvim-lualine/lualine.nvim>
 require("lualine").setup({
 	options = {
+		theme = 'nightfox',
 		globalstatus = true,
-		component_separators = { left = "|", right = "|" },
-		section_separators = { left = "", right = "" },
 	},
 })
