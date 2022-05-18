7eb7bb0f27808137789d3a4af43748e5e2e4bfcc
hehelego <2364261262@qq.com>
2022-05-18 19:46:34 +0800

    upd(fish): add a few more alias

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -5,19 +5,22 @@ set -gx EDITOR nvim
 set -gx VISUAL nvim
 set -gx MANPAGER "nvim +Man!"
 
+# disable less history
 set -gx LESSHISTFILE "/dev/null"
+
 
 # SECTION: alias and abbr
 #
-alias ls="exa"
-alias ll="exa --long --group-directories-first"
-alias la="exa --long --all --group-directories-first"
-alias tree="exa --tree"
-alias tree-list="exa --tree --long --all"
+alias   ls="exa              --group-directories-first"
+alias   ll="exa --long       --group-directories-first"
+alias   la="exa --long --all --group-directories-first"
+alias   lt="exa --tree"
+alias  lta="exa --tree --long --all"
 alias fzff="fd -HI --type file      --exclude .git | fzf --multi --cycle --preview='stat {}'"
 alias fzfd="fd -HI --type directory --exclude .git | fzf --multi --cycle --preview='exa -F1 {}'"
-alias bat="bat --pager=less"
-alias cat="bat --plain"
+alias  bat="bat --pager=less"
+alias  cat="bat --plain"
+alias   ip="ip -color"
 
 
 # SECTION: zoxide
@@ -43,6 +46,7 @@ end
 function fish_mode_prompt
   fish_vi_mode_prompt
 end
+
 
 # SECTION: key-bindings
 #
