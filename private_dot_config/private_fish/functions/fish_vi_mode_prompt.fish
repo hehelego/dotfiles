60c3fa1e4ca4e0cc373246ad93c5c7f12c8254f7
hehelego <2364261262@qq.com>
2022-05-28 22:35:57 +0800

    upd(fish): vim mode prompt

--- private_dot_config/private_fish/functions/fish_vi_mode_prompt.fish
+++ private_dot_config/private_fish/functions/fish_vi_mode_prompt.fish
@@ -1,20 +1,21 @@
 function fish_vi_mode_prompt
+  set_color --dim purple;          echo -n '['
   switch $fish_bind_mode
     case default
-      set_color --bold red
-      echo -n 'N'
+      set_color --bold red;        echo -n 'N'
     case insert
-      set_color --bold green
-      echo -n 'I'
+      set_color --bold green;      echo -n 'I'
     case replace_one
-      set_color --bold green
-      echo -n 'R'
+      set_color --bold green;      echo -n 'R'
+    case replace
+      set_color --bold brown;      echo -n 'R'
     case visual
-      set_color --bold brmagenta
-      echo -n 'V'
+      set_color --bold brmagenta;  echo -n 'V'
     case '*'
-      set_color --bold red
-      echo -n '?'
+      set_color --bold red;        echo -n '?'
   end
+  set_color --dim purple;          echo -n ']'
+
+  # restore
   set_color normal
 end
