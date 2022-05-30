edd0a6a928c4f54a57c7e77bf73cf0136b8ec308
hehelego <2364261262@qq.com>
2022-05-30 16:16:13 +0800

    upd(spinach fzf menu): simplify code; clean up

--- private_dot_config/spinach-menu/spinach_fzf_menu.py
+++ private_dot_config/spinach-menu/spinach_fzf_menu.py
@@ -13,6 +13,7 @@ class Config:
 
 
 class Helper:
+
     @staticmethod
     def log_file() -> IO[str]:
         return open(Config.logfile_path, 'a')
@@ -21,21 +22,18 @@ class Helper:
     def log(pre: str, msg: Any) -> None:
         with Helper.log_file() as f:
             print(pre, file=f, end=':\n')
-            if type(msg) == str:
-                print(msg, file=f)
-            else:
-                pprint(msg, stream=f)
+            pprint(msg, stream=f)
             print('', file=f)
 
     @staticmethod
-    def info(abs: str, msg: Any) -> None:
+    def info(summary: str, message: Any) -> None:
         ts = datetime.now().strftime('%H:%M:%S')
-        Helper.log(f'INFO(ts={ts}): {abs}', msg)
+        Helper.log(f'INFO(ts={ts}): {summary}', message)
 
     @staticmethod
-    def err(abs: str, msg: Any) -> None:
+    def err(summary: str, message: Any) -> None:
         ts = datetime.now().strftime('%H:%M:%S')
-        Helper.log(f'ERROR(ts={ts}): {abs}', msg)
+        Helper.log(f'ERROR(ts={ts}): {summary}', message)
 
     @staticmethod
     def readfile(path: str) -> str:
@@ -44,47 +42,59 @@ class Helper:
 
     @staticmethod
     def shell_run(cmd: str) -> None:
-        subprocess.run(['fish', '-c', cmd])
+        subprocess.check_call(['fish', '-c', cmd])
 
     T = TypeVar('T')
 
     @staticmethod
-    def fzf_select(src: list[T], multi: Optional[bool] = True, preview_cmd: Optional[str] = None, prompt: Optional[str] = '> ') -> list[T]:
-        input_file = tempfile.NamedTemporaryFile(
-            prefix='/tmp/spinach_i3_sysctrl.py', mode='w+')
-        output_file = tempfile.NamedTemporaryFile(
-            prefix='/tmp/spinach_i3_sysctrl.py', mode='w+')
+    def fzf_select(src: list[T],
+                   multi: Optional[bool] = True,
+                   preview_cmd: Optional[str] = None,
+                   prompt: Optional[str] = '> ') -> list[T]:
+
         src_map = {str(i): i for i in src}
+
+        def tmpf():
+            return tempfile.NamedTemporaryFile(
+                prefix='/tmp/spinach_qutepy.fzf',
+                mode='w+',
+            )
+
+        input_file = tmpf()
+        output_file = tmpf()
+
         for i in src_map.keys():
             print(i, file=input_file)
         input_file.flush()
-        input_file.seek(0)
 
-        fzf_cmd = ['fzf']
-        if multi:
-            fzf_cmd.append('--multi')
+        fzf_opts = ['fzf']
+        fzf_opts.append('--multi' if multi else '--no-multi')
         if preview_cmd:
-            fzf_cmd.extend(['--preview', preview_cmd])
-        if prompt:
-            fzf_cmd.extend(['--prompt', prompt])
+            fzf_opts.append(f"--preview '{preview_cmd}'")
+        fzf_opts.append(f'--prompt "{prompt}"')
 
-        Helper.log('command', fzf_cmd)
-        subprocess.run(
-            fzf_cmd, stdin=input_file, stdout=output_file)
+        fzf_cmd = ' '.join(fzf_opts)
+        Helper.log('fzf command', fzf_cmd)
+
+        subprocess.check_call([
+            'fish', '-c',
+            f'cat {input_file.name} | {fzf_cmd} > {output_file.name}'
+        ])
+
         output_file.seek(0)
         selected = [
-            src_map[i]
-            for i in output_file.read().split('\n')
-            if i in src_map
+            src_map[i] for i in output_file.read().split('\n') if i in src_map
         ]
+        Helper.log('selected:', selected)
+
         output_file.close()
         input_file.close()
 
-        Helper.log('selected', selected)
         return selected
 
 
 class Action:
+
     def __init__(self, cmd: str) -> None:
         self.cmd = cmd
 
@@ -93,14 +103,24 @@ class Action:
         see https://stackoverflow.com/questions/10408816/how-do-i-use-the-nohup-command-without-getting-nohup-out
         see https://stackoverflow.com/questions/20646519/how-to-spawn-a-new-independent-process-in-python
         '''
-        Helper.info('running', self.cmd)
-        subprocess.Popen(['nohup', 'fish', '-c', self.cmd],
-                         stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        if self.cmd:
+            Helper.info('running', self.cmd)
+            subprocess.Popen(
+                [
+                    'nohup',
+                    'fish',
+                    '-c',
+                    self.cmd,
+                ],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+            )
 
 
 class MenuEntry:
+
     def __init__(self, key: str, desc: str) -> None:
-        assert('/' not in key and len(key) > 0)
+        assert ('/' not in key and len(key) > 0)
         self.key = key
         self.desc = desc
 
@@ -114,25 +134,28 @@ class MenuEntry:
         return []
 
     def get_action(self) -> Action:
-        raise NotImplementedError("abstract MenuEntry has not action")
+        return Action('')
 
-    def _expand(self, path: EntryPath, cond: Callable[[MenuEntry], bool]) -> dict[EntryPath, MenuEntry]:
+    def expand(
+            self, path: EntryPath,
+            cond: Callable[[MenuEntry], bool]) -> dict[EntryPath, MenuEntry]:
         path = path.join(EntryPath.from_key(self.key))
-        d = dict()
+        result: dict[EntryPath, MenuEntry] = {}
         if cond(self):
-            d |= {path: self}
-        for s in self.get_son():
-            d |= s._expand(path, cond)
-        return d
+            result |= {path: self}
+        for son in self.get_son():
+            result |= son.expand(path, cond)
+        return result
 
     def expand_leafs(self) -> dict[EntryPath, MenuEntry]:
-        return self._expand(EntryPath.empty_path(), lambda e: e.is_leaf())
+        return self.expand(EntryPath.empty_path(), lambda e: e.is_leaf())
 
     def expand_all(self) -> dict[EntryPath, MenuEntry]:
-        return self._expand(EntryPath.empty_path(), lambda _: True)
+        return self.expand(EntryPath.empty_path(), lambda _: True)
 
 
 class EntryPath:
+
     def __init__(self, path: list[str]) -> None:
         self.path = path
 
@@ -145,9 +168,9 @@ class EntryPath:
     def __hash__(self) -> int:
         return hash(str(self))
 
-    def __eq__(self, o: object) -> bool:
-        if isinstance(o, EntryPath):
-            return o.path == self.path
+    def __eq__(self, obj: object) -> bool:
+        if isinstance(obj, EntryPath):
+            return obj.path == self.path
         return False
 
     @staticmethod
@@ -170,7 +193,7 @@ class EntryPath:
         self.path.extend(other.path)
 
     def join(self, other: EntryPath) -> EntryPath:
-        return EntryPath(self.path+other.path)
+        return EntryPath(self.path + other.path)
 
     def pop_head(self) -> EntryPath:
         return EntryPath.from_list(self.path[1:])
@@ -178,14 +201,14 @@ class EntryPath:
     def as_list(self) -> list[str]:
         return self.path[:]
 
-    def _find(self, cur: MenuEntry, p: int) -> Optional[MenuEntry]:
-        if p >= len(self.path) or self.path[p] != cur.key:
+    def _find(self, cur: MenuEntry, idx: int) -> Optional[MenuEntry]:
+        if idx >= len(self.path) or self.path[idx] != cur.key:
             return None
-        p += 1
-        if p == len(self.path):
+        idx += 1
+        if idx == len(self.path):
             return cur
         for i in cur.get_son():
-            res = self._find(i, p)
+            res = self._find(i, idx)
             if res is not None:
                 return res
         return None
@@ -195,27 +218,27 @@ class EntryPath:
 
 
 class MenuParser:
+
     @staticmethod
     def from_object(doc: dict[str, Any]) -> MenuEntry:
         keys = list(doc.keys())
-        assert(len(keys) == 1)
+        assert len(keys) == 1
         key = keys[0]
         obj = doc[key]
-        assert('desc' in obj)
-        assert(('son' in obj) ^ ('action' in obj))
+        assert 'desc' in obj
+        assert ('son' in obj) ^ ('action' in obj)
 
         desc = obj['desc']
         if 'son' in obj:
             son = [MenuParser.from_object(i) for i in obj['son']]
             return EntryInner(key, desc, son)
-        else:
-            return EntryLeaf(key, desc, Action(obj['action']))
+        return EntryLeaf(key, desc, Action(obj['action']))
 
 
 class EntryLeaf(MenuEntry):
+
     def __init__(self, key: str, desc: str, action: Action) -> None:
-        self.key = key
-        self.desc = desc
+        super().__init__(key, desc)
         self.act = action
 
     def is_leaf(self) -> bool:
@@ -226,6 +249,7 @@ class EntryLeaf(MenuEntry):
 
 
 class EntryInner(MenuEntry):
+
     def __init__(self, key: str, desc: str, son: list[MenuEntry]) -> None:
         super().__init__(key, desc)
         self.son = son
@@ -238,6 +262,7 @@ class EntryInner(MenuEntry):
 
 
 class Navigator(ABC):
+
     def __init__(self, root: MenuEntry) -> None:
         self.root = root
         return
@@ -246,9 +271,9 @@ class Navigator(ABC):
         return self.root
 
     def re_root(self, new_root_path: EntryPath) -> None:
-        r = new_root_path.find(self.root)
-        assert(r is not None)
-        self.root = r
+        root = new_root_path.find(self.root)
+        assert root is not None
+        self.root = root
 
     @abstractmethod
     def get_cur_path(self) -> EntryPath:
@@ -268,6 +293,7 @@ class Navigator(ABC):
 
 
 class ExpandedMode(Navigator):
+
     def __init__(self, root: MenuEntry) -> None:
         super().__init__(root)
         self.root = root
@@ -291,6 +317,7 @@ class ExpandedMode(Navigator):
 
 
 class LevelMode(Navigator):
+
     def __init__(self, root: MenuEntry) -> None:
         super().__init__(root)
         self.cur = self.root
@@ -324,6 +351,7 @@ class LevelMode(Navigator):
 
 
 class MixedMode(Navigator):
+
     def __init__(self, root: MenuEntry) -> None:
         super().__init__(root)
         self.path = EntryPath.from_key(self.root.key)
@@ -340,52 +368,57 @@ class MixedMode(Navigator):
         return self.path
 
     def choices(self) -> list[str]:
-        groups = [f'GROUP\t{k}' for (
-            k, v) in self.subs.items() if v.is_inner()]
-        actions = [f'ACTION\t{k}' for (
-            k, v) in self.subs.items() if v.is_leaf()]
-        return groups+actions
+        groups = [
+            f'GROUP\t{k}' for (k, v) in self.subs.items() if v.is_inner()
+        ]
+        actions = [
+            f'ACTION\t{k}' for (k, v) in self.subs.items() if v.is_leaf()
+        ]
+        return groups + actions
 
     def full_path(self, choice: str) -> EntryPath:
-        path = EntryPath.from_rawpath(choice[choice.index('\t')+1:])
+        path = EntryPath.from_rawpath(choice[choice.index('\t') + 1:])
         return self.path.join(path.pop_head())
 
     def select(self, choice: str) -> MenuEntry:
-        path = EntryPath.from_rawpath(choice[choice.index('\t')+1:])
+        path = EntryPath.from_rawpath(choice[choice.index('\t') + 1:])
         self.path.join_inplace(path.pop_head())
         self.cur = self.subs[path]
         self.subs = self.cur.expand_all()
         return self.cur
 
 
-def main_select(finder: Navigator, shell_cmd_self: str, prompt: Optional[str]) -> None:
+def main_select(finder: Navigator, shell_cmd_self: str,
+                prompt: Optional[str]) -> None:
+
     def _update_path(file: IO[str], path: EntryPath) -> None:
         file.seek(0)
         file.truncate(0)
         file.write(str(path))
         file.flush()
 
-    tmpf = tempfile.NamedTemporaryFile(
-        mode='w+', prefix='/tmp/spinach-fzf-menu-path')
-
-    preview_cmd = f'{shell_cmd_self} --mode preview --pathfile {tmpf.name} --choice {{}}'
-    while True:
-        _update_path(tmpf.file, finder.get_cur_path())
-        choices = finder.choices()
-        selected = Helper.fzf_select(choices, False, preview_cmd, prompt)
-        if len(selected) > 0:
-            path = selected[0]
-            entry = finder.select(path)
-            if entry.is_leaf():
-                entry.get_action().run()
+    with tempfile.NamedTemporaryFile(
+            mode='w+',
+            prefix='/tmp/spinach-fzf-menu-path',
+    ) as tmpf:
+        preview_cmd = f'{shell_cmd_self} --mode preview --pathfile {tmpf.name} --choice {{}}'
+        while True:
+            _update_path(tmpf.file, finder.get_cur_path())
+            choices = finder.choices()
+            selected = Helper.fzf_select(choices, False, preview_cmd, prompt)
+            if len(selected) > 0:
+                path = selected[0]
+                entry = finder.select(path)
+                if entry.is_leaf():
+                    entry.get_action().run()
+                    break
+            else:
                 break
-        else:
-            break
-    tmpf.close()
-    return
 
 
-def main_preview(finder: Navigator, preview_callback: Callable[[EntryPath, MenuEntry], str], pathfile: str,  choice: str):
+def main_preview(finder: Navigator,
+                 preview_callback: Callable[[EntryPath, MenuEntry],
+                                            str], pathfile: str, choice: str):
     path = EntryPath.from_rawpath(Helper.readfile(pathfile))
     Helper.info('before re-root', path)
     finder.re_root(path)
@@ -394,7 +427,11 @@ def main_preview(finder: Navigator, preview_callback: Callable[[EntryPath, MenuE
     print(preview_callback(path, entry), end='')
 
 
-def main(argv: list[str], load: Callable[[], MenuEntry], preview: Callable[[EntryPath, MenuEntry], str], log_file: str, prompt: str = '> '):
+def main(argv: list[str],
+         load: Callable[[], MenuEntry],
+         preview: Callable[[EntryPath, MenuEntry], str],
+         log_file: str,
+         prompt: str = '> '):
     '''
     - argv:
         arguments passed to fzf-menu
@@ -409,28 +446,34 @@ def main(argv: list[str], load: Callable[[], MenuEntry], preview: Callable[[Entr
     '''
 
     Config.logfile_path = log_file
-    ap = argparse.ArgumentParser()
-    ap.add_argument('--mode',
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument(
+        '--mode',
+        type=str,
+        choices=['select', 'preview'],
+        default='select',
+        metavar='mode',
+        help=
+        'one of [select, preview]. run for fzf-preview command or selection')
+    arg_parser.add_argument(
+        '--show',
+        type=str,
+        choices=['expanded', 'level', 'mixed'],
+        default='level',
+        metavar='display_mode',
+        help='one of [expanded, level, mixed]. how to display the menu tree')
+    arg_parser.add_argument('--pathfile',
                     type=str,
-                    choices=['select', 'preview'],
-                    default='select',
-                    metavar='mode',
-                    help='one of [select, preview]. run for fzf-preview command or selection')
-    ap.add_argument('--show', type=str,
-                    choices=['expanded', 'level', 'mixed'],
-                    default='level',
-                    metavar='display_mode',
-                    help='one of [expanded, level, mixed]. how to display the menu tree')
-    ap.add_argument('--pathfile', type=str,
                     default='',
                     metavar='pathfile',
                     help='path to file storing the current path of selector')
-    ap.add_argument('--choice', type=str,
+    arg_parser.add_argument('--choice',
+                    type=str,
                     default='',
                     metavar='choice',
                     help='the hover entry path(only used for preview mode)')
 
-    arg = ap.parse_args(argv[1:])
+    arg = arg_parser.parse_args(argv[1:])
     mode: str = arg.mode
     show: str = arg.show
     pathfile: str = arg.pathfile
@@ -444,6 +487,6 @@ def main(argv: list[str], load: Callable[[], MenuEntry], preview: Callable[[Entr
     }[show](menu)
 
     if mode == 'preview':
-        main_preview(finder, preview, pathfile,  choice)
+        main_preview(finder, preview, pathfile, choice)
     else:
-        main_select(finder, 'python '+' '.join(argv), prompt)
+        main_select(finder, 'python ' + ' '.join(argv), prompt)
