f085bbf592b8dc7b8562fff79d384ed5f8a86456
hehelego <2364261262@qq.com>
2022-06-04 11:58:05 +0800

    upd(nvim): nvim-cmp config, debounce

--- private_dot_config/nvim/lua/config/langs/cmp/init.lua
+++ private_dot_config/nvim/lua/config/langs/cmp/init.lua
@@ -5,15 +5,40 @@
 -- Credit <https://github.com/LunarVim/Neovim-from-scratch/blob/master/lua/user/cmp.lua>
 -- Credit <https://github.com/t-troebst/config.nvim/blob/master/lua/user/completion.lua>
 
-local lspkind = require("lspkind")
+local cmp = require("cmp")
 local luasnip = require("luasnip")
+local lspkind = require("lspkind")
 -- load snippets for luasnip
 require("luasnip.loaders.from_vscode").lazy_load()
 
-local cmp = require("cmp")
+-- See <https://github.com/hrsh7th/nvim-cmp/issues/598>
+local cmp_delay = vim.opt.timeoutlen:get()
+local cmp_timer = vim.loop.new_timer()
+local function debounce()
+	cmp_timer:stop()
+	cmp_timer:start(
+		cmp_delay,
+		0,
+		vim.schedule_wrap(function()
+			cmp.complete({ reason = cmp.ContextReason.Auto })
+		end)
+	)
+end
+
+local cmp_debounce_grp = vim.api.nvim_create_augroup("cmp_debounce", {})
+vim.api.nvim_create_autocmd({ "TextChangedI", "CmdlineChanged" }, {
+	pattern = "*",
+	callback = debounce,
+	group = cmp_debounce_grp,
+	desc = "nvim-cmp auto-completion debounce",
+})
+
 local cmp_keymaps = {
 	-- Ctrl-Space to trigger completion
-	["<C-Space>"] = cmp.mapping.complete(),
+	["<C-Space>"] = cmp.mapping(function()
+		cmp_timer:stop()
+		cmp.complete()
+	end),
 	-- Return to confirm completion
 	["<CR>"] = cmp.mapping.confirm({ select = true }),
 	--  navigating the completion menu
@@ -22,7 +47,10 @@ local cmp_keymaps = {
 	["<C-p>"] = cmp.mapping.select_prev_item(),
 	["<C-n>"] = cmp.mapping.select_next_item(),
 	-- Ctrl-e to abort completion
-	["<C-e>"] = cmp.mapping.abort(),
+	["<C-e>"] = cmp.mapping(function()
+		cmp_timer:stop()
+		cmp.abort()
+	end),
 	-- Tab: next option | next snippet slot | fallback
 	["<Tab>"] = cmp.mapping(function(fallback)
 		if cmp.visible() then
@@ -95,26 +123,4 @@ cmp.setup.cmdline(":", {
 		{ name = "path" },
 		{ name = "cmdline" },
 	},
-})
-
--- See <https://github.com/hrsh7th/nvim-cmp/issues/598>
-local delay = vim.opt.timeoutlen:get()
-local timer = vim.loop.new_timer()
-local function debounce()
-	timer:stop()
-	timer:start(
-		delay,
-		0,
-		vim.schedule_wrap(function()
-			cmp.complete({ reason = cmp.ContextReason.Auto })
-		end)
-	)
-end
-
-local cmp_debounce_grp = vim.api.nvim_create_augroup("cmp_debounce", {})
-vim.api.nvim_create_autocmd({ "TextChangedI", "CmdlineChanged" }, {
-	pattern = "*",
-	callback = debounce,
-	group = cmp_debounce_grp,
-	desc = "nvim-cmp auto-completion debounce",
 })
