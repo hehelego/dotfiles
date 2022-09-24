b7d6ac7cb8323635c3ef8c0a8af628fce3061d68
hehelego <2364261262@qq.com>
2022-09-24 14:11:21 +0800

    upd(nvim): add nvim-cmp capabilities for auto-completion

--- private_dot_config/nvim/lua/config/langs/mason/init.lua
+++ private_dot_config/nvim/lua/config/langs/mason/init.lua
@@ -86,6 +86,7 @@ local server_conf = {
 mason_lsp.setup_handlers({
 	function(server_name)
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
+		capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
 		-- NOTE: issue on offset encoding
 		-- see <https://github.com/jose-elias-alvarez/null-ls.nvim/issues/428>
