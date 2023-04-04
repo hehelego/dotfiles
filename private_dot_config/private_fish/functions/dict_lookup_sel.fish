70268c874c9859bb57c0497e94938c78ff5c34c6
hehelego <2364261262@qq.com>
2023-04-04 11:30:47 +0800

    upd(fish): dictionary lookup function with `dictd` and `dict`

--- private_dot_config/private_fish/functions/dict_lookup_sel.fish
+++ private_dot_config/private_fish/functions/dict_lookup_sel.fish
@@ -2,12 +2,18 @@ function dict_lookup_sel
   set sel_file "/tmp/selected_text"
   cat /dev/null > $sel_file
 
-  if test "x11" = "$XDG_SESSION_TYPE"
-    xclip -o -selection primary > $sel_file
+  if test "x11"     = "$XDG_SESSION_TYPE"
+    xclip    -o -selection primary > $sel_file
   else if "wayland" = "$XDG_SESSION_TYPE"
-    wl-paste --primary > $sel_file
+    wl-paste    --primary          > $sel_file
   end
 
-  cat $sel_file | ydcv -n &
-	disown $last_pid
+
+  dict (cat $sel_file) > /tmp/dictd.result 2> /tmp/dictd.error
+  if test $status -eq 0
+    notify-send "dictd query result" "$(cat /tmp/dictd.result)" &
+  else
+    notify-send "dictd query error"  "$(cat /tmp/dictd.error)"  &
+  end
+  disown $last_pid
 end
