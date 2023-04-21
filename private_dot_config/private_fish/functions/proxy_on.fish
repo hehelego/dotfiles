936c7a857e51a673a4b57cd5333bb08510931826
hehelego <2364261262@qq.com>
2023-04-21 08:57:38 +0800

    upd(fish): `no_proxy` env, convert space-separated to comma-separated

--- private_dot_config/private_fish/functions/proxy_on.fish
+++ private_dot_config/private_fish/functions/proxy_on.fish
@@ -4,6 +4,6 @@ function proxy_on
 	set -gx https_proxy     "$http_proxy"
 	set -gx ftp_proxy       "$http_proxy"
 	set -gx rsync_proxy     "$http_proxy"
-	set -gx no_proxy        "localhost" "127.0.0.1" "localaddress" ".localdomain.com"
+	set -gx no_proxy        "localhost,127.0.0.1,localaddress,.localdomain.com"
 end
 
