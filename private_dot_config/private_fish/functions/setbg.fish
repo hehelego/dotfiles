37cce1a30c19a5ab9c8cfc73fb124f12a452a2a6
hehelego <2364261262@qq.com>
2023-07-24 22:35:07 -0700

    upd(fish): `setbg` invokes `feh` with `--no-fehbg` argument

--- private_dot_config/private_fish/functions/setbg.fish
+++ private_dot_config/private_fish/functions/setbg.fish
@@ -1,7 +1,7 @@
 function setbg
 	if test "$XDG_SESSION_TYPE" = "x11"
 		set bg_img /usr/share/wallpapers/Opal/contents/images/3840x2160.png
-		feh --bg-fill $bg_img
+		feh --bg-fill --no-fehbg $bg_img
 	end
 	if test "$XDG_SESSION_TYPE" = "wayland"
 		set bg_img /usr/share/backgrounds/sway/Sway_Wallpaper_Blue_1920x1080.png
