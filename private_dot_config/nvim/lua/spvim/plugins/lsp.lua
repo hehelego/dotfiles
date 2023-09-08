c45ab6621d33f61181c478a12d2ffbd1aec90ae4
hehelego <2364261262@qq.com>
2023-09-08 18:23:25 -0500

    upd(nvim): update plugins; change sign column icons

--- private_dot_config/nvim/lua/spvim/plugins/lsp.lua
+++ private_dot_config/nvim/lua/spvim/plugins/lsp.lua
@@ -1,9 +1,9 @@
 local function lspclient_setup()
 	local diagnostic_signs = {
-		{ name = "DiagnosticSignError", text = "" },
-		{ name = "DiagnosticSignWarn", text = "" },
-		{ name = "DiagnosticSignHint", text = "" },
-		{ name = "DiagnosticSignInfo", text = "" },
+		{ name = "DiagnosticSignError", text = "" },
+		{ name = "DiagnosticSignWarn", text = "" },
+		{ name = "DiagnosticSignHint", text = "" },
+		{ name = "DiagnosticSignInfo", text = "" },
 	}
 
 	for _, sign in ipairs(diagnostic_signs) do
