2cbea33b35d0b34a2617213027fbc7fbdbe39180
hehelego <hahalegao@gmail.com>
2023-11-01 11:52:58 -0500

    upd(fish): set keymap only for interactive shell session

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -6,7 +6,7 @@ set -gx VISUAL nvim
 set -gx MANPAGER "nvim +Man!"
 
 # disable less pager history
-set -gx LESSHISTFILE "/dev/null"
+set -gx LESSHISTFILE /dev/null
 # disable node repl history
 set -gx NODE_REPL_HISTORY ""
 
@@ -14,18 +14,18 @@ set -gx NODE_REPL_HISTORY ""
 
 # SECTION: alias and abbr
 #
-alias   ls="eza              --group-directories-first"
-alias   ll="eza --long       --group-directories-first"
-alias   la="eza --long --all --group-directories-first"
-alias   lt="eza --tree"
-alias  lta="eza --tree --long --all"
-alias  fdf="fd -HI --type file      --exclude .git --strip-cwd-prefix"
-alias  fdd="fd -HI --type directory --exclude .git --strip-cwd-prefix"
+alias ls="eza              --group-directories-first"
+alias ll="eza --long       --group-directories-first"
+alias la="eza --long --all --group-directories-first"
+alias lt="eza --tree"
+alias lta="eza --tree --long --all"
+alias fdf="fd -HI --type file      --exclude .git --strip-cwd-prefix"
+alias fdd="fd -HI --type directory --exclude .git --strip-cwd-prefix"
 alias fzff="fdf | fzf --multi --preview='stat {}'"
 alias fzfd="fdd | fzf --multi --preview='exa -F1 {}'"
-alias  bat="bat --pager=less"
-alias  cat="bat --plain"
-alias   ip="ip -color=auto"
+alias bat="bat --pager=less"
+alias cat="bat --plain"
+alias ip="ip -color=auto"
 
 
 # SECTION: zoxide
@@ -36,24 +36,27 @@ zoxide init fish | source
 
 # SECTION: fzf
 #
-set -gx FZF_DEFAULT_COMMAND "fdf"
+set -gx FZF_DEFAULT_COMMAND fdf
 set -gx FZF_DEFAULT_OPTS "--cycle --reverse --height=40%"
 
 
-# SECTION: prompt
-#
-## load starship
-if not test "$TERM" = "linux"
-  set -gx STARSHIP_CONFIG ~/.config/starship/config.toml
-  starship init fish | source
-end
-## add vim mode prompt
-function fish_mode_prompt
-  fish_vi_mode_prompt
-end
+if status is-interactive
+    # SECTION: prompt
+    #
+    ## load starship
+    if not test "$TERM" = linux
+        set -gx STARSHIP_CONFIG ~/.config/starship/config.toml
+        starship init fish | source
+    end
+
+    ## add vim mode prompt
+    function fish_mode_prompt
+        fish_vi_mode_prompt
+    end
 
 
-# SECTION: key-bindings
-#
-fish_hybrid_key_bindings
-fzf_key_bindings
+    # SECTION: key-bindings
+    #
+    fish_hybrid_key_bindings
+    fzf_key_bindings
+end
