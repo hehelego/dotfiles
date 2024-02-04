e1345d63629160c28027a4f2d82676a8ef522bc1
hehelego <hahalegao@gmail.com>
2024-02-04 19:03:19 +0800

    upd(fish): ocaml env, eza arguments

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -9,20 +9,21 @@ set -gx MANPAGER "nvim +Man!"
 set -gx LESSHISTFILE /dev/null
 # disable node repl history
 set -gx NODE_REPL_HISTORY ""
-
+# ocaml library installation path
+set -x OCAMLPATH $HOME/.opam/default/lib
 
 
 # SECTION: alias and abbr
 #
-alias ls="eza              --group-directories-first"
-alias ll="eza --long       --group-directories-first"
-alias la="eza --long --all --group-directories-first"
-alias lt="eza --tree"
-alias lta="eza --tree --long --all"
+alias ls="eza -F              --group-directories-first"
+alias ll="eza -F --long       --group-directories-first"
+alias la="eza -F --long --all --group-directories-first"
+alias lt="eza -F --tree"
+alias lta="eza -F --tree --long --all"
 alias fdf="fd -HI --type file      --exclude .git --strip-cwd-prefix"
 alias fdd="fd -HI --type directory --exclude .git --strip-cwd-prefix"
 alias fzff="fdf | fzf --multi --preview='stat {}'"
-alias fzfd="fdd | fzf --multi --preview='exa -F1 {}'"
+alias fzfd="fdd | fzf --multi --preview='eza -F -1 --color=always {}'"
 alias bat="bat --pager=less"
 alias cat="bat --plain"
 alias ip="ip -color=auto"
@@ -30,7 +31,7 @@ alias ip="ip -color=auto"
 
 # SECTION: zoxide
 #
-set -gx _ZO_FZF_OPTS "--cycle --reverse --height=40% --preview='exa -F1 {2..}'"
+set -gx _ZO_FZF_OPTS "--cycle --reverse --height=40% --preview='eza -F -1 --color=always {2..}'"
 zoxide init fish | source
 
 
