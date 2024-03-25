0c43794d852378698aa48cc986f1856cc265508a
hehelego <hahalegao@gmail.com>
2024-03-25 17:13:59 +0800

    fix(fish functions): correct equality testing

--- private_dot_config/private_fish/functions/dict_lookup_sel.fish
+++ private_dot_config/private_fish/functions/dict_lookup_sel.fish
@@ -2,11 +2,13 @@ function dict_lookup_sel
   set sel_file "/tmp/selected_text"
   cat /dev/null > $sel_file
 
-  if test "x11"     = "$XDG_SESSION_TYPE"
+  if test      "x11"     = "$XDG_SESSION_TYPE"
     xclip    -o -selection primary > $sel_file
-  else if "wayland" = "$XDG_SESSION_TYPE"
-    wl-paste    --primary          > $sel_file
+  else if test "wayland" = "$XDG_SESSION_TYPE"
+    wl-paste             --primary > $sel_file
   end
+
+  cat $sel_file
 
 
   dict (cat $sel_file) > /tmp/dictd.result 2> /tmp/dictd.error
