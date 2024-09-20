e9762f4a3959defdf117ba21f2703e28634ea1a3
hehelego <hahalegao@gmail.com>
2024-09-20 14:57:32 +0800

    upd(nvim): agda-mode cornelis keymap

--- private_dot_config/nvim/lua/spvim/maps_cmds/lsp.lua
+++ private_dot_config/nvim/lua/spvim/maps_cmds/lsp.lua
@@ -43,25 +43,75 @@ vim.api.nvim_create_user_command("Format", buf_fmt_async, {
 	desc = "lsp document formatting",
 })
 
--- For Cornelis Agda-mode
+-- For Coqtail
+vim.api.nvim_create_autocmd({ "FileType" }, {
+	pattern = { "coq" },
+	callback = function(args)
+		wk.add({
+			buffer = args.buf,
+			{ "<leader>c", group = "Coqtail" },
+		})
+	end,
+})
+-- For Cornelis Agda-Mode
+local function agda_cmd()
+	vim.ui.select({
+		-- global
+		"CornelisLoad",
+		"CornelisGoals",
+		"CornelisRestart",
+		"CornelisAbort",
+		"CornelisSolve ",
+		"CornelisGoToDefinition",
+		"CornelisPrevGoal",
+		"CornelisNextGoal",
+		"CornelisQuestionToMeta",
+		"CornelisInc",
+		"CornelisDec",
+		"CornelisCloseInfoWindows",
+		-- goal
+		"CornelisGive",
+		"CornelisRefine",
+		"CornelisElaborate",
+		"CornelisAuto",
+		"CornelisMakeCase",
+		"CornelisTypeContext",
+		"CornelisTypeInfer",
+		"CornelisTypeContextInfer",
+		"CornelisNormalize",
+		"CornelisWhyInScope",
+		"CornelisHelperFunc",
+	}, {
+		prompt = "Cornelis Agda-Mode:",
+		format_item = function(item)
+			return "run " .. item
+		end,
+	}, function(item, idx)
+		local _ = idx
+		pcall(vim.cmd, item)
+	end)
+end
 vim.api.nvim_create_autocmd({ "FileType" }, {
 	pattern = { "agda" },
 	callback = function(args)
-		local opt = { buffer = args.buf, silent = false, desc = "cornelis agda-mode" }
-		vim.keymap.set("n", "<leader>cl", "<cmd>CornelisLoad<CR>", opt)
-		vim.keymap.set("n", "<leader>cg", "<cmd>CornelisGoals<CR>", opt)
-		vim.keymap.set("n", "<leader>cc", "<cmd>CornelisCloseInfoWindows<CR>", opt)
-		vim.keymap.set("n", "<leader>cr", "<cmd>CornelisRefine<CR>", opt)
-		vim.keymap.set("n", "<leader>cd", "<cmd>CornelisMakeCase<CR>", opt)
-		vim.keymap.set("n", "<leader>c,", "<cmd>CornelisTypeContext<CR>", opt)
-		vim.keymap.set("n", "<leader>c.", "<cmd>CornelisTypeContextInfer<CR>", opt)
-		vim.keymap.set("n", "<leader>cn", "<cmd>CornelisSolve<CR>", opt)
-		vim.keymap.set("n", "<leader>ca", "<cmd>CornelisAuto<CR>", opt)
-		vim.keymap.set("n", "gd", "<cmd>CornelisGoToDefinition<CR>", opt)
-		vim.keymap.set("n", "[/", "<cmd>CornelisPrevGoal<CR>", opt)
-		vim.keymap.set("n", "]/", "<cmd>CornelisNextGoal<CR>", opt)
-		vim.keymap.set("n", "<C-A>", "<cmd>CornelisInc<CR>", opt)
-		vim.keymap.set("n", "<C-X>", "<cmd>CornelisDec<CR>", opt)
+		wk.add({
+			buffer = args.buf,
+			{ "<leader>c", group = "Cornelis" },
+			{ "<leader>cc", agda_cmd, desc = "commands" },
+			{ "<leader>ca", "<cmd>CornelisAuto<CR>", desc = "auto" },
+			{ "<leader>cl", "<cmd>CornelisLoad<CR>", desc = "load&check" },
+			{ "<leader>cg", "<cmd>CornelisGoals<CR>", desc = "list goals" },
+			{ "<leader>cr", "<cmd>CornelisRefine<CR>", desc = "refine" },
+			{ "<leader>cd", "<cmd>CornelisMakeCase<CR>", desc = "case split" },
+			{ "<leader>c,", "<cmd>CornelisTypeContext<CR>", desc = "goal&context" },
+			{ "<leader>c.", "<cmd>CornelisTypeContextInfer<CR>", desc = "inferred type" },
+			{ "<leader>cn", "<cmd>CornelisSolve<CR>", desc = "solve constraints" },
+			{ "gd", "<cmd>CornelisGoToDefinition<CR>", desc = "goto definition" },
+			{ "[c", "<cmd>CornelisPrevGoal<CR>", desc = "previous goal" },
+			{ "]c", "<cmd>CornelisNextGoal<CR>", desc = "next goal" },
+			{ "<C-A>", "<cmd>CornelisInc<CR>", desc = "inc" },
+			{ "<C-X>", "<cmd>CornelisDec<CR>", desc = "dec" },
+		})
 	end,
 	desc = "cornelis agda-mode",
 	group = vim.api.nvim_create_augroup("agda-cornelis-group", {}),
